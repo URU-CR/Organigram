74291cf008d805549cb2fe8fb827ccc681551368
aleskrupa109 <ales.krupa@gmail.com>
2026-09-14 14:41:54 +0200

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // Přístupové údaje k Supabase – Project Settings → API
 window.CONFIG = {
-  SUPABASE_URL: 'https://tsdeenlpaboczbkdnvyi.supabase.co/rest/v1',
+  SUPABASE_URL: 'https://tsdeenlpaboczbkdnvyi.supabase.co',
   SUPABASE_ANON_KEY: 'sb_publishable_92PbQWm7cGbLj7EY59Nf2Q_UNM_CwS5',
 };
